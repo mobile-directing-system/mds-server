aa0cf00e24bb73b7cc898766eb033ec047f4c473
Lennart Altenhof <l.f.altenhof@gmail.com>
2022-06-14 23:14:53 +0200

    fix(go-shared): fix default-limit not applied at from-request
    Fixes the default-limit not being applied when building pagination params from an HTTP request. This makes the default-limit apply at database-query level but not being shown in the HTTP response.

--- services/go/shared/pagination/pagination.go
+++ services/go/shared/pagination/pagination.go
@@ -17,7 +17,7 @@ type Paginated[T any] struct {
 	// Total is the total amount of available entries.
 	Total int `json:"total"`
 	// Limit that was applied for retrieving the Entries.
-	Limit nulls.Int `json:"limit"`
+	Limit int `json:"limit"`
 	// Offset that was applied for retrieving the Entries.
 	Offset int `json:"offset"`
 	// Retrieves is the amount of entries in Entries.
@@ -29,7 +29,7 @@ type Paginated[T any] struct {
 // Params is a container for common ways of retrieving paginated results.
 type Params struct {
 	// Limit for the amount of retrieved results.
-	Limit nulls.Int `json:"limit"`
+	Limit int `json:"limit"`
 	// Offset is the offset for retrieving results with the set Limit.
 	Offset int `json:"offset"`
 	// OrderBy is an optional field to order results by.
